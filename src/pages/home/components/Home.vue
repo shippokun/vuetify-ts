79a9c444a3c448572e618e940d2e0ad8ea0b5a5a
shippokun <okashi0tabetai@gmail.com>
2020-06-25 23:21:39 +0900

    chore: change component design
    Container component と Presentational component にした

---
+++ src/pages/home/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { HelloWorld } from "../../hello-world";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
