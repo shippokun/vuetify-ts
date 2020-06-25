ea5ccacefec67a3fa03802b641d03c94b148ad00
shippokun <okashi0tabetai@gmail.com>
2020-06-25 23:43:31 +0900

    feat: add vuetify

--- src/pages/hello-world/components/HelloWorld.vue
+++ src/pages/hello-world/components/HelloWorld.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-row class="text-center">
+    <v-col cols="12">
+      <v-img
+        :src="require('@/assets/logo.svg')"
+        class="my-3"
+        contain
+        height="200"
+      />
+      <h1>{{ msg }}</h1>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
