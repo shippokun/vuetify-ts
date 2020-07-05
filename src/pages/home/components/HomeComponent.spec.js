53cbd38105b73b7d14f0aeed919fe3c70e3924f1
shippokun <okashi0tabetai@gmail.com>
2020-07-05 18:43:26 +0900

    fix: 🐛 現状でかけてないtestファイルを削除

--- src/pages/home/components/HomeComponent.spec.js
+++
@@ -1,6 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import HomeComponent from './HomeComponent.vue';
-
-describe('HomeComponent.vue', () => {
-  const wrapper = shallowMount(HomeComponent);
-});
--- src/pages/home/containers/HomeContainer.spec.js
+++
@@ -1,6 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import HomeContainer from './HomeContainer.vue';
-
-describe('HomeContainer.vue', () => {
-  const wrapper = shallowMount(HomeContainer);
-});
